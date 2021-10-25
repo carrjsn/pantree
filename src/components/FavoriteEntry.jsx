522af46998f26d36b28a0aa3acbba571c5ec869a
Jason Carr <carrjsn@gmail.com>
2021-10-25 11:50:49 -0700

    favorite css updates

--- src/components/FavoriteEntry.jsx
+++ src/components/FavoriteEntry.jsx
@@ -3,13 +3,14 @@ import React from 'react';
 
 const FavoriteEntry = (props) => (
   <div className='favorite-recipe'>
-    <img className='fav-image' src={props.recipe.imageUrl} width='80' height='80'/>
-    <div className='fav-title'>{props.recipe.title}</div>
-    <div><a className='fav-link' href='#'>Get Recipe</a></div>
-    <div className='remove-favorite-button'>
-      <button name='remove-favorite' onClick={() => props.removeFavorite(props.recipe.spoonId)}>Remove favorite</button>
+    <img className='fav-image' src={props.recipe.imageUrl} />
+    <div className='fav-info'>
+      <div className='fav-title'>{props.recipe.title}</div>
+      <div><a className='fav-link' href='#'>Get Recipe</a></div>
+      <div className='remove-favorite-button'>
+        <button name='remove-favorite' onClick={() => props.removeFavorite(props.recipe.spoonId)}>Remove favorite</button>
+      </div>
     </div>
-
   </div>
 )
 
